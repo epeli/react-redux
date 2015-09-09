02259996ea94888c31b75dbf4a885cab4a753247
Esa-Matti Suuronen <esa-matti@suuronen.org>
2015-09-09 10:53:26 +0300

    New test 'should not render the wrapped component when mapState does not produce change'

--- test/components/connect.spec.js
+++ test/components/connect.spec.js
@@ -1147,5 +1147,38 @@ describe('React', () => {
       wrapper.setState({ value: 1 });
       expect(target.props.statefulValue).toEqual(1);
     });
+
+    it('should not render the wrapped component when mapState does not produce change', () => {
+      const store = createStore(stringBuilder);
+      let renderCalls = 0;
+      let mapStateCalls = 0;
+
+      @connect(() => {
+        mapStateCalls++;
+        return {}; // no change!
+      })
+      class Container extends Component {
+        render() {
+          renderCalls++;
+          return <Passthrough {...this.props} />;
+        }
+      }
+
+      TestUtils.renderIntoDocument(
+        <ProviderMock store={store}>
+          <Container />
+        </ProviderMock>
+      );
+
+      expect(renderCalls).toBe(1);
+      expect(mapStateCalls).toBe(1);
+
+      store.dispatch({ type: 'APPEND', body: 'a'});
+
+      // After store a change mapState has been called
+      expect(mapStateCalls).toBe(2);
+      // But render is not because it did not make any actual changes
+      expect(renderCalls).toBe(1);
+    });
   });
 });
